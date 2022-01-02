2c82815e4fe5f79ba496d31816e115d1a4ab21d5
Francis Akortia <akpeneakortia@outlook.com>
2022-01-02 22:41:33 +0000

    basic operators

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,6 @@
 /* 
+/////////////////////////////////////
+// Variables
 let js = 'amazing';
 console.log(40 + 8 + 23 - 10);
 
@@ -25,6 +27,8 @@ let job2 = "teacher";
 
 console.log(myFirstJob); 
 
+/////////////////////////////////////////
+// Data Types
 let javascriptIsFun = true;
 console.log(javascriptIsFun);
 
@@ -44,8 +48,9 @@ year = 1988;
 console.log(typeof year);
 
 console.log(typeof null);
-*/
 
+////////////////////////////////////////////
+// let, const, and var
 let age = 30;
 age = 31;
 
@@ -59,3 +64,36 @@ job = 'teacher';
 
 lastName = 'Akortia';
 console.log(lastName);
+*/
+/////////////////////////////////////
+// Basic Operators
+
+//Math Operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2*2*2
+
+const firstName = "Jonas";
+const lastName = 'Schmedtmann';
+console.log(firstName + ' ' + lastName);
+
+//Assignment operators
+let x = 10 + 5;
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100;
+x++; // x = x + 1
+x--;
+x--;
+console.log(x);
+
+//Comparison Operators
+console.log(ageJonas > ageSarah);
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
