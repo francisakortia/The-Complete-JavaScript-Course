fb20e07a2afa3f8f33f639195d0b432f868d8be0
Francis Akortia <akpeneakortia@outlook.com>
2022-01-02 21:56:07 +0000

    datatypes

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,4 @@
+/* 
 let js = 'amazing';
 console.log(40 + 8 + 23 - 10);
 
@@ -6,8 +7,10 @@ console.log(23);
 
 let firstName = "Bob";
 console.log(firstName);
+console.log(firstName);
+console.log(firstName);
 
-
+//variable name conventions
 let jonas_matilda = "JM";
 let $function = 27;
 
@@ -20,4 +23,25 @@ let myCurrentJob = "Teacher";
 let job1 = "programmer";
 let job2 = "teacher";
 
-console.log(myFirstJob);
+console.log(myFirstJob); 
+*/
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof "Francis");
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1988;
+console.log(typeof year);
+
+console.log(typeof null);
