add55cfa2e38a0350bcd11a8d0ce5d255742800e
Francis Akortia <akpeneakortia@outlook.com>
2022-03-16 01:17:08 +0000

    equality operators

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -232,6 +232,8 @@ console.log(n);
 
 */
 
+/*
+
 // 5 falsey values: 0, "", undefined, null, NaN
 
 console.log(Boolean(0));
@@ -253,3 +255,27 @@ if (height) {
 } else {
     console.log('Height is UNDEFINED')
 }
+
+*/
+
+const age = '18';
+if (age === 18) console.log('You just became an adult 🙌 (strict)');
+
+if (age == 18) console.log('You just became an adult 🙌 (loose)');
+
+const favourite = Number(prompt("What's your favourite number"));
+
+console.log(favourite);
+console.log(typeof (favourite));
+
+if (favourite === 23) {
+    console.log('Cool! 23 is an amazing number');
+} else if (favourite === 7) {
+    console.log('7 is also a cool number');
+} else if (favourite === 9) {
+    console.log('9 is also a cool number');
+} else {
+    console.log('Number is not 23 or 7 or 9');
+}
+
+if (favourite !== 23) console.log('Wy not 23');
