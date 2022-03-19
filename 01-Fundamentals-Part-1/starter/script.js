505ba51129880ac97e2ab3948bbdb6e5c45d165a
Francis Akortia <akpeneakortia@outlook.com>
2022-03-19 13:11:05 +0000

    coding challenge #4

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -378,6 +378,8 @@ if (day === 'monday') {
 
 */
 
+/*
+
 const age = 15;
 age >= 18 ? console.log('I like to drink wine 🍷') : console.log("I like to drink water 💦");
 
@@ -393,3 +395,13 @@ if (age >= 18) {
 console.log(drink2);
 
 console.log(`I like to drink ${age >= 18 ? 'wine 🍷' : 'water 💦'}`);
+
+*/
+
+
+// CODING CHALLENGE 4
+const bill = 275;
+
+const tip = bill >= 50 && bill <= 300 ? 0.15 * bill : 0.20 * bill;
+
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
