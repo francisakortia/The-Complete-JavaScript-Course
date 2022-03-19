a194be6910a4bc5cf0df7a583a712bfa7c99c2c5
Francis Akortia <akpeneakortia@outlook.com>
2022-03-19 11:33:11 +0000

    the switch statement

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -309,6 +309,8 @@ if (hasDriversLicense && hasGoodVision && !isTired) {
 
 */
 
+/*
+
 // CODING CHALLENGE #3"
 
 // 1
@@ -329,3 +331,45 @@ if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
     console.log('Teams scored below 100');
 }
 
+*/
+
+const day = 'thursday';
+
+switch (day) {
+    case 'monday':
+        console.log('Plan course structure');
+        console.log('Go to coding meetup');
+        break;
+    case 'tuesday':
+        console.log('Prepare theory videos');
+        break;
+    case 'wednesday':
+    case 'thursday':
+        console.log('Write code examples');
+        break
+    case 'friday':
+        console.log('Record videos');
+        break;
+    case 'saturday':
+    case 'sunday':
+        console.log('Enjoy the weekend');
+        break;
+    default:
+        console.log('Not a valid day');
+        break;
+}
+
+if (day === 'monday') {
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
+} else if (day === 'tuesday') {
+    console.log('Prepare theory videos');
+} else if (day === 'wednesday' || day === 'thursday') {
+    console.log('Write code examples');
+} else if (day === 'friday') {
+    console.log('Record videos');
+} else if (day === 'saturday' || day === 'sunday') {
+    console.log('Enjoy the weekend');
+} else {
+    console.log('Not a valid day');
+}
