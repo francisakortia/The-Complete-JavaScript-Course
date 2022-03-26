e6101c744a643a4db0449c5d53ee438b8470c7d7
Francis Akortia <akpeneakortia@outlook.com>
2022-03-26 21:14:01 +0000

    function declarations vs expressions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -13,6 +13,8 @@ if (hasDriversLicense) console.log('I can drive :D');
 
 */
 
+/*
+
 function logger() {
     console.log('My name is Francis');
 }
@@ -34,3 +36,19 @@ const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
 
 const num = Number('23');
+*/
+
+// Function Declaration
+function calcAge1(birthYear) {
+    return 2037 - birthYear;
+
+}
+const age1 = calcAge1(1991);
+
+// Function Expression
+const calcAge2 = function (birthYear) {
+    return 2037 - birthYear;
+}
+const age2 = calcAge2(1991);
+
+console.log(age2, age2);
