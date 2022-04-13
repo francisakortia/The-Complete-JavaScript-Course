c6178a44f48c059d010876a1f7457adbb6280ae4
Francis Akortia <akpeneakortia@outlook.com>
2022-04-13 18:12:15 +0000

    review functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -73,6 +73,8 @@ console.log(yearsUntilRetirement(1991, 'Francis'));
 console.log(yearsUntilRetirement(1980, 'Bob'));
 */
 
+/*
+
 function cutFruitPieces(fruit) {
     return fruit * 4;
 }
@@ -85,4 +87,26 @@ function fruitProcessor(apples, oranges) {
     return juice;
 }
 console.log(fruitProcessor(2, 3));
+*/
 
+const calcAge = function (birthYear) {
+    return 2037 - birthYear;
+}
+
+const yearsUntilRetirement = function (birthYear, firstName) {
+    const age = calcAge(birthYear);
+    const retirement = 65 - age;
+
+    if (retirement > 0) {
+        console.log(`${firstName} retires in ${retirement} years`);
+        return retirement;
+    } else {
+        console.log(`${firstName} has already retired 🎉`)
+        return -1;
+    }
+
+    
+   
+}
+console.log(yearsUntilRetirement(1991, 'Francis'));
+console.log(yearsUntilRetirement(1950, 'Mike'));
